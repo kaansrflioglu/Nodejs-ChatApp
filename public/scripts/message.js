3d84e54d026447e963e4f4f82165d8e189c5c410
Kaan <balos-kaan@outlook.com>
2024-03-23 17:52:38 +0300

    update message.js

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -4,8 +4,6 @@ const form = document.getElementById("form");
 const input = document.getElementById("message");
 const messages = document.getElementById("messages");
 
-let lastUsername = null;
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = input.value.trim();
@@ -20,24 +18,6 @@ function scrollToBottom() {
   const chatContainer = document.getElementById("messages");
   chatContainer.scrollTop = chatContainer.scrollHeight;
 }
-
-/*
-socket.on("chat message", (data) => {
-  const otherUser = `<img src="assets/people.png" width="20" height="20"><strong> ${data.user}</strong><br>`;
-  const sessionUser = `<strong> ${data.user}</strong><img src="assets/people.png" width="20" height="20"><br>`;
-  const item = document.createElement("div");
-  if (data.user === userName) {
-    item.innerHTML = `${sessionUser}${element.message}`;
-    item.classList.add("chat-bubble-self");
-  } else {
-    item.innerHTML = `${otherUser}${data.message}`;
-    item.classList.add("chat-bubble");
-  }
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-  scrollToBottom();
-});
-*/
 
 socket.on("chat message", (data) => {
   const sessionUser = `<strong> ${data.user}</strong>&nbsp;<img src="assets/people.png" width="20" height="20"><br>`;
@@ -54,7 +34,6 @@ socket.on("chat message", (data) => {
   window.scrollTo(0, document.body.scrollHeight);
   scrollToBottom();
 });
-
 
 socket.on("old_messages", (msg) => {
   msg.forEach((element) => {
